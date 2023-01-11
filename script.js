718d590bc55d734a0520daa54840819e6abec6c5
Prince mishra <119921857+princemishra9@users.noreply.github.com>
2023-01-11 21:00:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const textbox = document.getElementId("evaluatedText");
+const lettercount = document.getElementId("letterCount");
+textbox.addEventListener('input', () => {
+    const letterCount = textbox.value.length;
+
+    letterCountElement.textContent = letterCount;
+});
